414ddaf92ef44824460b5ef217843eb9bdf2dcc8
jasonramirezlee-photolocationmap <jasonramirezlee@gmail.com>
2025-11-09 11:27:42 -0800

    Update page.tsx

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,6 +1,6 @@
-'use client' // THIS MUST BE THE FIRST LINE
-
 // /vercel/path0/app/app/page.tsx
+
+'use client' // THIS MUST BE THE FIRST LINE
 
 import { useState } from 'react'
 import { Home, Book, Hash, Package, Camera, DollarSign, Settings } from 'lucide-react'
